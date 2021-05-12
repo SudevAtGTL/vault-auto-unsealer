d9e79bf159ac88bf171c66ff912dcc8493f81f5d
Sudev Gandhi <SudevAtGTL@users.noreply.github.com>
2021-05-12 23:10:06 +0200

    Updated auto-unsealer
    Aim is to just unseal the vault with the provided key. Nothing more nothing else.

--- vault-auto-unsealer.rb
+++ vault-auto-unsealer.rb
@@ -17,52 +17,10 @@ end
 
 require "vault"
 
-puts "Checking if Vault is initialized."
-
-if !Vault.sys.init_status.initialized?
-  puts "Vault is not initialized. Attempting to initialize the Vault server."
-
-  pgp_key_path = ENV["PGP_KEY_PATH"]
-
-  if pgp_key_path.nil?
-    abort "Environment variable PGP_KEY_PATH must be set to the path of a file containing a Base64-encoded (but not ASCII armored) OpenPGP public key that Vault's keys should be encrypted with."
-  end
-
-  pgp_key = File.read(pgp_key_path).chomp
-
-  response = Vault.sys.init(
-    pgp_keys: [pgp_key],
-    root_token_pgp_key: pgp_key,
-    secret_shares: 1,
-    secret_threshold: 1,
-  )
-
-  puts <<EOS
-Vault initialized successfully.
-The following values are Base64 encoded and encrypted with OpenPGP.
-
-Unseal key: #{response.keys_base64.first}
-Root token: #{response.root_token}
-
-Redeploy vault-auto-unsealer with the environment variable UNSEAL_KEY set to the decrypted unseal key.
-EOS
-else
-  puts "Vault was already initialized."
-end
-
 unseal_key = ENV["UNSEAL_KEY"]
 
 if unseal_key.nil? || unseal_key == ""
   abort "Environment variable UNSEAL_KEY must be set to the decrypted Vault unseal key."
-end
-
-if unseal_key.bytesize != 64
-  puts <<EOS
-Placeholder UNSEAL_KEY detected.
-vault-auto-unsealer will now sleep until terminated with SIGTERM, so that Kubernetes will not try to restart it before an operator can redeploy it with UNSEAL_KEY set.
-EOS
-
-  sleep
 end
 
 puts "Entering main control loop. Vault will be checked every 30 seconds and unsealed if it is found sealed."
